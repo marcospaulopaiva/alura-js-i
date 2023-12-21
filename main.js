356e897bbc3e1078a908fc807e56c233911406da
marcospaulopaiva <marcos.poo@gmail.com>
2023-12-21 18:59:10 -0300

    Aula 5 - Eventos e lógicas

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor invalido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
@@ -13,7 +18,18 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function() {
-        tocaSom(idAudio)
-    };
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+       
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
